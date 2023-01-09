37e50a61dcb20357e9fa081948204c9b94c015e2
hdhyun <hdhyun@mz.co.kr>
2023-01-09 17:41:25 +0900

    프로젝트 세팅

---
+++ FarmnCook/src/main/java/FarmnCook/FarmnCook/FarmnCookApplication.java
@@ -0,0 +1,13 @@
+package FarmnCook.FarmnCook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmnCookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmnCookApplication.class, args);
+	}
+
+}
---
+++ FarmnCook/src/test/java/FarmnCook/FarmnCook/FarmnCookApplicationTests.java
@@ -0,0 +1,13 @@
+package FarmnCook.FarmnCook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmnCookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
