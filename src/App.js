1a860bc3bb19eb101b562b8560f57b5c04a9464b
dahyunhan <dollae7020@gmail.com>
2023-01-29 13:55:52 +0900

    로그인 UI코드 복사

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Test from "./Test";
+import Example from "./login/Login";
 // import "tailwindcss/tailwind.css";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Test />} />
+      <Route path="/" element={<Example />} />
     </Routes>
   );
 }
